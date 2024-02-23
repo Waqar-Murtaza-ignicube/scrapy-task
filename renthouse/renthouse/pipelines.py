c02dc8a0ea86a60cec431307a50c3bdee95d8e39
Waqar-Murtaza-ignicube <waqar.murtaza@ignicube.com>
2024-02-23 15:51:01 +0500

    changes

--- renthouse/renthouse/pipelines.py
+++ renthouse/renthouse/pipelines.py
@@ -10,28 +10,38 @@ class RenthousePipeline:
         country = adapter.get("country")
         adapter["country"] = country.capitalize()
 
-        address = adapter.get("address")
-        adapter["address"] = address.strip()
+        address_item = adapter.get("address")
+        address = ''.join(address_item).strip()
+        adapter["address"] = address
 
         postal_code = adapter.get("postal_code")
         if postal_code is not None:
             adapter["postal_code"] = postal_code.strip()
 
-        surface = adapter.get("surface")
-        if surface is not None:
+        surface_item = adapter.get("surface")
+        if surface_item is not None:
+            surface = ''.join(surface_item).strip()
             numerical_value = int(surface.split()[0])
             square_meters_int = numerical_value
             adapter["surface"] = f"{square_meters_int} m\u00B2"
 
-        bedrooms = adapter.get("bedrooms")
-        if bedrooms is not None:
+        bedrooms_item = adapter.get("bedrooms")
+        if bedrooms_item is not None:
+            bedrooms = ''.join(bedrooms_item).strip()
             adapter["bedrooms"] = int(bedrooms)
 
-        pet_friendly = adapter.get("pet_friendly")
-        if pet_friendly == "Not allowed":
-            adapter["pet_friendly"] = "No"
-        elif pet_friendly == "Allowed":
-            adapter["pet_friendly"] = "Yes"
+        bath_item = adapter.get("bath")
+        if bath_item is not None:
+            bathrooms = ''.join(bath_item).strip()
+            adapter["bath"] = int(bathrooms)
+
+        pets_item = adapter.get("pet_friendly")
+        if pets_item is not None:
+            pet_friendly = ''.join(pets_item).strip()
+            if pet_friendly == "Not allowed":
+                adapter["pet_friendly"] = "No"
+            elif pet_friendly == "Allowed":
+                adapter["pet_friendly"] = "Yes"
 
         photo = adapter.get("photo")
         adapter["photo"] = photo.split(" ")[0]
@@ -43,8 +53,9 @@ class RenthousePipeline:
         description = adapter["description"]
         adapter["description"] = ' '.join(space.strip() for space in description if space.strip())
 
-        income_requirement = adapter.get("income_requirement")
-        if income_requirement is not None:
+        income_item = adapter.get("income_requirement")
+        if income_item is not None:
+            income_requirement = ''.join(income_item).strip()
             adapter["income_requirement"] = float(income_requirement)
 
         return item
--- renthouse/renthouse/spiders/rent.py
+++ renthouse/renthouse/spiders/rent.py
@@ -9,7 +9,7 @@ class RentSpider(scrapy.Spider):
     start_urls = ["https://www.iamexpat.nl/housing/rentals"]
 
     def parse(self, response):
-
+        """parse method to scrape from response of website"""
         property_link = response.css("li.property")
         for link in property_link:
             property_uri = link.css("a.article__link::attr(href)").get()
@@ -29,35 +29,43 @@ class RentSpider(scrapy.Spider):
 
             yield response.follow(next_page_uri, callback=self.parse)
 
+    def extract_fields(self, response, field_key):
+        """method to merge the same fields functionality"""
+        if field_key in ('bedrooms', 'surface'):
+            field_value = response.xpath(f"//span[contains(@class,'label--{field_key}')]"
+                                         "/../text()")
+        else:
+            field_value = response.xpath(f"//div[@class='field']/div[contains(text(),'{field_key}'"
+                                         ")]/../text()")
+
+        return field_value.getall() if len(field_value) > 1 else None
+
     def property_parser(self, response, items):
         """parsing the response from property url"""
-        postal_code = response.xpath("//div[@class='field']/div[contains(text(),'Address:')]"
-                                     "/../text()")
-        postal_code_avail = postal_code[1].get().split(",")
+
+        # making a dic for items having similar selectors
+        field_items = {
+            'Address:': 'address',
+            'Deposit:': 'deposit',
+            'Pets:': 'pets_allowed',
+            'Bathrooms:': 'bathrooms',
+            'bedrooms': 'bedrooms',
+            'surface': 'surface'
+        }
+        extracted_items = {}
+        for field_key, field_value in field_items.items():
+            extracted_items[field_value] = self.extract_fields(response, field_key)
+
+        #extracting postal code
+        postal_code_avail = ''.join(extracted_items.get('address')).split(",")
         has_postal_code = postal_code_avail[1] if len(postal_code_avail) > 1 else None
 
-        deposit = response.xpath("//div[@class='field']/div[contains(text(),'Deposit:')]/../text()")
-        has_deposit = deposit[1].get().strip() if len(deposit) > 1 else None
-
-        pets_allowed = response.xpath("//div[@class='field']/div[contains(text(),'Pets:')]"
-                                      "/../text()")
-        has_pets = pets_allowed[1].get().strip() if len(pets_allowed) > 1 else None
-
-        bathrooms = response.xpath("//div[contains(text(),'Bathrooms:')]/../text()")
-        has_baths = bathrooms[1].get().strip() if len(bathrooms) > 1 else None
-
-        bedrooms = response.css(".field:has(span.label--bedrooms)::text")
-        has_bedrooms = bedrooms[1].get().strip() if len(bedrooms) > 1 else None
-
-        surface = response.css(".field:has(span.label--surface)::text")
-        has_surface = surface[1].get().strip() if len(surface) > 1 else None
-
-
+        #extracting description
         desc = response.css("div.property__body-section h2")
         desc_p_elements = desc.xpath("//div[contains(text(), 'Description:')]/../../p/text()")
 
         if desc_p_elements:
-            description = desc_p_elements
+            description = desc_p_elements.getall()
         else:
             description = desc.xpath("//div[contains(text(), 'Description:')]"
                                      "/../../text()").getall()
@@ -68,18 +76,17 @@ class RentSpider(scrapy.Spider):
         house_item["url"] = response.url
         house_item["country"] = response.css("li.main__country a::text").get()
         house_item["city"] = response.css(".breadcrumb li.last a::text").get()
-        house_item["address"] = response.xpath("//div[contains(text(),'Address:')]"
-                                               "/../text()")[1].get()
+        house_item["address"] = extracted_items.get('address')
         house_item["postal_code"] = has_postal_code
-        house_item["surface"] = has_surface
-        house_item["bedrooms"] = has_bedrooms
+        house_item["surface"] = extracted_items.get('surface')
+        house_item["bedrooms"] = extracted_items.get('bedrooms')
         house_item["furnished"] = items['furnished']
-        house_item["bath"] = has_baths
-        house_item["pet_friendly"] = has_pets
+        house_item["bath"] = extracted_items.get('bathrooms')
+        house_item["pet_friendly"] = extracted_items.get('pets_allowed')
         house_item["photo"] = response.css(".gallery img::attr(srcset)").get()
         house_item["price"] = response.css("p.price-wrapper .property__price::text").get()
         house_item["description"] = description
-        house_item["income_requirement"] = has_deposit
+        house_item["income_requirement"] = extracted_items.get('deposit')
         house_item["realtor"] = response.css(".property__main-info a::text").get()
         house_item["realtor_link"] = response.css(".property__main-info a::attr(href)").get()
         house_item["website"] = items['website_url']
